6fb1d16bfd38e687508142db96e70674f51e18f7
js3720 <115711559+js3720@users.noreply.github.com>
2023-08-25 18:09:02 +0900

    Delete final project-LJY directory
    err

--- final project-LJY/main.js
+++
@@ -1,8 +0,0 @@
-const searchBtn = document.getElementsByClassName("search");
-const topSearch = document.querySelector(".top-searchr");
-
-topSearch.style.dispaly="none";
-searchBtn.addEventListener("click", function(){
-    
-    
-})
