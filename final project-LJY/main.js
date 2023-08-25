ad34e978f8de8c881329628782560c2794b569b5
jeongheum park <likegym2@naver.com>
2023-08-25 17:52:49 +0900

    no message

---
+++ final project-LJY/main.js
@@ -0,0 +1,8 @@
+const searchBtn = document.getElementsByClassName("search");
+const topSearch = document.querySelector(".top-searchr");
+
+topSearch.style.dispaly="none";
+searchBtn.addEventListener("click", function(){
+    
+    
+})
