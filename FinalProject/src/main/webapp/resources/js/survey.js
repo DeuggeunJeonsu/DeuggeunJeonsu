9d45af781c7994284ed632820ccbfaa22500dca5
JHJ <jsung_9@naver.com>
2023-09-06 17:50:16 +0900

    2023-09-06 학원 끝나고

--- FinalProject/src/main/webapp/resources/js/survey.js
+++ FinalProject/src/main/webapp/resources/js/survey.js
@@ -3,24 +3,90 @@ const leg = document.getElementById("ex_chk2");
 const btn = document.getElementById("bodyCheck");
 
 
-const input = document.getElementById("rangeInput1");
+const input = document.getElementById("rangeblue");
+
+const fristCheck = document.getElementsByClassName("fristCheck");
+const fristList = document.getElementsByClassName("bodycheck0");
 const next = document.getElementsByClassName("bodycheck1");
+const nextleg = document.getElementsByClassName("bodycheck2");
+
+
+const header = document.getElementById("fristHeader");
+const nextHeader = document.getElementsByClassName("svHeader2")[0];
 
 let len = next.length;
 
-ub.addEventListener("change", () => {
 
-    if(ub.checked == true){
+if (leg.checked == true && ub.checked == true) {
+
+
+}
+
+
+for (let i of fristCheck) {
+ console.log(i);
+    i .addEventListener("click", () => {
         
-        btn.addEventListener("click", () => {
-    
-            for(let i = 0 ; i < len; i++){
-    
-                //next[i].innerHTML="";
-                next[0].remove();
-            }
-        })
-        
-    }
-})
+        if (ub.checked == true || leg.checked == true) {
+            
+            btn.addEventListener("click", () => {
+                console.log("ww");
+
+                if (leg.checked == true && ub.checked == true) {
+
+                    alert("하나만 선택 해주세요.");
+
+                    leg.checked = false;
+                    ub.checked = false;
+
+                    return;
+
+                } else if (ub.checked == true) {
+
+                    for (let i = 0; i < next.length; i++) {
+
+                        next[i].style.display = "contents";
+
+
+                        fristList[i].style.display = "none";
+
+                        header.innerHTML = "상체를 하는 날이군요! 그 중 어느 부위를 하시나요?";
+
+                       /*  rangeInput.style.transition = "background";
+                        rangeInput.style.transition = ""; */
+
+                        input.style.transitionDuration = "2s, 1s";
+                        input.style.width = "60%";
+
+                        btn.innerHTML = "결과확인";
+
+
+                    }
+
+                } else if (leg.checked == true) {
+
+                    for (let i = 0; i < next.length; i++) {
+
+                        nextleg[i].style.display = "contents";
+
+
+                        fristList[i].style.display = "none";
+
+                        header.innerHTML = "하체를 하는 날이군요! 그 중 어느 부위를 하시나요?";
+
+                        input.style.transitionDuration = "2s, 1s";
+                        input.style.width = "60%";
+
+                        btn.innerHTML = "결과확인";
+                    }
+
+                }
+
+            })
+
+        }
+    })
+
+}
+
 
