9b813eb81da1301737a50152139024b0ea6a7475
jeongheum park <likegym2@naver.com>
2023-09-12 08:33:58 +0900

    no message

--- FinalProject/src/main/java/com/jeonsu/deuggeun/board/controller/ShareBoardController.java
+++ FinalProject/src/main/java/com/jeonsu/deuggeun/board/controller/ShareBoardController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ShareBoard {
+public class ShareBoardController {
 	
 	@RequestMapping("/shareBoard")
 	public String shareBoardList() {
