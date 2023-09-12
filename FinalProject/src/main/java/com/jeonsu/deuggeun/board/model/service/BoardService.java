b5708bca2c84fddf3800f0e01bb35612fbf1ebd6
ash <dkstngus0615@gmail.com>
2023-09-12 11:36:07 +0900

    ㅠㅠ

--- FinalProject/src/main/java/com/jeonsu/deuggeun/board/model/service/BoardService.java
+++ FinalProject/src/main/java/com/jeonsu/deuggeun/board/model/service/BoardService.java
@@ -1,10 +1,11 @@
-package com.jeonsu.deuggeun.board.service;
+package com.jeonsu.deuggeun.board.model.service;
 
 import java.util.List;
 import java.util.Map;
 
 public interface BoardService {
 
+	// 게시판 코드 조회
 	List<Map<String, Object>> selectBoardTypeList();
 
 }
