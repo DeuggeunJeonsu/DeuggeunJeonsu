f5473a049ea02df66a31ac6a27be3d5e8f0508ce
이민주 <minjooo0116@gmail.com>
2023-09-30 10:04:25 +0900

    라스트 커밋

--- FinalProject/src/main/webapp/resources/js/main.js
+++ FinalProject/src/main/webapp/resources/js/main.js
@@ -1,39 +1,10 @@
 // 채팅 카운트 최신화
 let chattingCount;
-//let chattingCountIsRunning = false; 
-//if (chattingCountIsRunning) clearInterval(chattingCount); // 실행중이면 초기화
-
-// 로그인 후 채팅아이콘이 존재 할 때만
-if($('#chatting-count').length){
-    console.log("채팅아이콘 존재");
-    realTimeUpdates();
-}
-else console.log("채팅아이콘 없음");
-
-function realTimeUpdates(){
-    chattingCount = setInterval(function () {
-        $.ajax({
-            type: "GET",
-            url: "/chattingCount",
-            dataType: "text",
-            success: function(response) {
-                if (parseInt(response) > 0) {
-                    $('#chatting-count').text(response).show();
-                } else {
-                    $('#chatting-count').hide();
-                }
-            },
-            error: function(error) {
-                console.error("데이터 가져오기 오류: ", error);
-            }
-        });
-    }, 1000);
-    //chattingCountIsRunning = true;
-}
+$('#chatting-count').hide();
 
 /*검색 버튼 눌렀을 때 창 사라지고 나타남*/
 $(document).ready(function(){
-    // 돋보기 버튼 눌렀을 시 검색 창 나타나거나 사라짐  
+    // 돋보기 버튼 눌렀을 시 검색 창 나타나거나 사라짐
     $('.search').click( function (){
         $('.top-search').slideToggle();
     })
@@ -43,11 +14,11 @@ $(document).ready(function(){
         $('.top-search').slideUp();
     })
 
-    // 스크롤 여부 확인 후 header 변화 
+    // 스크롤 여부 확인 후 header 변화
     var didScroll = false; // 스크롤 여부를 확인하는 변수
     const headerNav = document.querySelector(".header-nav");
     const navContainer = document.querySelector(".nav-container")
-    const log01 = document.querySelector(".log01");  
+    const log01 = document.querySelector(".log01");
     const log02 = document.querySelector(".log02"); //스크롤내렸을 때 보이게 될 로고
     const body = document.querySelector("body")
 
@@ -58,7 +29,7 @@ $(document).ready(function(){
 
     // 메인페이지가 아닐때
     if(document.querySelector("[name='mainPage']") == null){
-    
+
         navContainer.classList.add("nav-sticky");
         headerNav.classList.remove("no-background");
         headerNav.classList.add("nav-container-bg")
@@ -69,7 +40,7 @@ $(document).ready(function(){
         return;
 
     }
-    
+
 
     setInterval(function() {
         if (didScroll) {
@@ -78,8 +49,7 @@ $(document).ready(function(){
         }
     }, 250);
 
-   
-    
+
 
     function hasScrolled() {
 
@@ -102,14 +72,14 @@ $(document).ready(function(){
             return;
         }
     }
-    
-    $(".schedule-scroll").click(function() {
-    // .tdl-btnArea 요소의 위치(offset)를 가져옵니다.
-    var tdlBtnAreaPosition = $("#schedule-area").offset().top - 130;
 
-    // 화면을 tdlBtnArea의 위치로 스크롤합니다.
-    $("html, body").animate({ scrollTop: tdlBtnAreaPosition }, 600); // 500은 스크롤 애니메이션의 속도(ms)를 나타냅니다. 필요에 따라 조절하세요.
-});
+    $(".schedule-scroll").click(function() {
+        // .tdl-btnArea 요소의 위치(offset)를 가져옵니다.
+        var tdlBtnAreaPosition = $("#schedule-area").offset().top - 130;
+
+        // 화면을 tdlBtnArea의 위치로 스크롤합니다.
+        $("html, body").animate({ scrollTop: tdlBtnAreaPosition }, 600); // 500은 스크롤 애니메이션의 속도(ms)를 나타냅니다. 필요에 따라 조절하세요.
+    });
 
 });
 
@@ -142,9 +112,13 @@ $(document).ready(function() {
 
 })();
 
-// 페이지가 준비되면 장바구니 수량 업데이트 함수를 호출
+// 페이지가 준비되면 업데이트 함수를 호출
 $(document).ready(function() {
-    updateShoppingCount();
+    // 로그인 후 장바구니, 채팅 아이콘이 존재 할 때만
+    if($('.count-area').length){
+        updateShoppingCount();
+        realTimeUpdates();
+    }
 });
 
 function updateShoppingCount() {
@@ -166,13 +140,25 @@ function updateShoppingCount() {
     });
 }
 
-
-
-
-
-
-
-
+function realTimeUpdates(){
+    chattingCount = setInterval(function () {
+        $.ajax({
+            type: "GET",
+            url: "/chattingCount",
+            dataType: "text",
+            success: function(response) {
+                if (parseInt(response) > 0) {
+                    $('#chatting-count').text(response).show();
+                } else {
+                    $('#chatting-count').hide();
+                }
+            },
+            error: function(error) {
+                console.error("데이터 가져오기 오류: ", error);
+            }
+        });
+    }, 500);
+}
 
 
 
