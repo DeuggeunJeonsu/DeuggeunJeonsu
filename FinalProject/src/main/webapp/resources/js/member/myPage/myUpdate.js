6bdd881fc5e109479165fca40b9301945b5ee930
LJY <complete0415@naver.com>
2023-10-01 22:00:13 +0900

    회원정보 수전 전화번호 인증 진행 중

--- FinalProject/src/main/webapp/resources/js/member/myPage/myUpdate.js
+++ FinalProject/src/main/webapp/resources/js/member/myPage/myUpdate.js
@@ -3,6 +3,7 @@
 const memberNickname = document.getElementById("memberNickname");
 const updateInfo = document.getElementById("updateInfo");
 const nickMessage = document.getElementById("nickMessage");
+const memberTel = document.getElementById("memberTel");
 
 if(updateInfo != null){
     initNickname = memberNickname.value;
@@ -124,10 +125,162 @@ if(updateInfo != null){
 
 }
 
+// ----------------------------------------------------------------------------
+// 전화번호 인증
 
+// 휴대폰 인증
+const memberTels = document.getElementsByName("memberTel");
+let memberTel2 = "";
 
+const sendSmsBtn = document.getElementById("sendSmsBtn");
+const smsTimerSpan = document.getElementById("smsTimer");
+
+const telAtKey = document.getElementById("tel-auth-key");
+const telAuthBtn = document.getElementById("tel-auth-btn");
+
+let smsTimer;
+let smsIsRunning = false;
+
+/* 휴대폰 인증 번호 버튼 클릭 시 */
+sendSmsBtn.addEventListener("click", (e) => {
+
+    memberTel2 = "";
+
+    for (let i of memberTels) {
+
+        /* 휴대폰 번호를 입력하지 않았을 때 */
+        if (i.value.trim().length == 0) {
+
+            Swal.fire({
+                icon: 'error',                     
+                title: '인증 번호 전송 실패',    
+                text: '휴대폰 번호를 입력해 주세요.', 
+            });
+
+            memberTel.focus();
+            return;
+        }
+
+        memberTel2 += i.value;
+    }
+
+    // 기존 쿠키 삭제
+    delete_cookie(memberTel + "_telAtKey");
+
+    // ajax 코드 작성
+    fetch("/member/sendSms", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: memberTel,
+    })
+        .then((response) => response.text())
+        .then((resultTelAtKey) => {
+
+            if (resultTelAtKey != "") {
+
+                // 타이머 세팅
+                if (smsIsRunning) clearInterval(smsTimer); // 이미 타이머가 실행중이면 초기화
+                startTimer(60 * 5 - 1, 1); // sms 타이머 함수 실행
+
+                // 1회 이상 전송시 버튼 문구 변경
+                e.target.innerText = "인증 번호 재전송";
+
+                // 5분간 유효한 쿠키 생성
+                set_cookie(memberTel + "_telAtKey", resultTelAtKey, 5);
+
+                // 안내문구 출력
+                Swal.fire({
+                    icon: 'success',                     
+                    title: '인증 번호 전송 성공',    
+                    text: '휴대폰을 확인해 주세요.', 
+                });
+
+                telAtKey.focus();
+
+            } else {
+
+                Swal.fire({
+                    icon: 'success',                     
+                    title: '인증 번호 전송 실패',    
+                    text: '휴대폰 번호를 다시 확인해 주세요.', 
+                });
+
+                memberTel.focus();
+            }
+        })
+        .catch((err) => {
+            console.log("예외 발생");
+            console.log(err);
+        });
+});
+
+/* 휴대폰 인증 결과 출력 영역 */
+const authMessage2 = document.getElementById("authMessage2");
+
+/* 휴대폰 인증 버튼 클릭 시 */
+telAuthBtn.addEventListener("click", () => {
+    const smsCookieValue = get_cookie(memberTel + "_telAtKey");
+
+    /* 인증 번호 전송을 안 눌렀을 때 */
+    if (smsCookieValue == null) {
+
+        Swal.fire({
+            icon: 'error',                     
+            title: '인증 번호를 전송해 주세요.',    
+        });
+
+        return;
+    }
+
+    /* 인증 번호를 입력 안 했을 때 */
+    if (telAtKey.value.trim().length == 0) {
+
+        Swal.fire({
+            icon: 'error',                     
+            title: '인증 번호를 입력해 주세요.',    
+        });
+
+        telAtKey.focus();
+        return;
+    }
+
+    /* 인증 성공 */
+    if (smsCookieValue == telAtKey.value) {
+
+        authMessage2.innerText = "인증되었습니다.";
+        authMessage2.classList.add("confirm");
+        authMessage2.classList.remove("error");
+        clearInterval(smsTimer);
+        smsTimerSpan.innerText = "";
+
+        checkObj.memberTel = true;
     
-    
+    /* 인증 실패 */
+    } else {
+        authMessage2.innerText = "인증 번호가 일치하지 않습니다.";
+        authMessage2.classList.add("error");
+        authMessage2.classList.remove("confirm");
 
+        checkObj.memberTel = false;
+        telAtKey.focus();
+    }
+});
 
+//쿠키 생성하는 함수
+function set_cookie(name, value, minute) {
+    let date = new Date();
+    date.setTime(date.getTime(date.toUTCString(Math.floor((new Date()).getTime() / 1000))));
+    date.setMinutes(date.getMinutes()+minute);
+    document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value) + ';expires=' + date.toUTCString() + ';path=/';
+}
 
+//쿠키 값 가져오는 함수
+function get_cookie(name) {
+    let value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return value? value[2] : null;
+}
+
+//쿠키 삭제하는 함수
+function delete_cookie(name) {
+    document.cookie = encodeURIComponent(name) + '=; expires=Thu, 01 JAN 1999 00:00:10 GMT;path=/';
+}
